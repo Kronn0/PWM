fa96354b723f64e3be7c009935dbc22b1839483b
raulm1906 <raulm1906@gmail.com>
2023-03-22 11:48:32 +0000

    Añadidos algunos comentarios

--- scripts/forms/saveFormData.js
+++ scripts/forms/saveFormData.js
@@ -1,5 +1,6 @@
 function SaveFormData () {
     const inputsList = document.getElementsByTagName("input");
+    // Guarda los datos del formulario salvo contraseñas
     for (i = 0; i < inputsList.length; i++) {
         if (inputsList[i].type != "password"){
             sessionStorage.setItem(inputsList[i].id,
--- scripts/forms/validatePassword.js
+++ scripts/forms/validatePassword.js
@@ -1,5 +1,4 @@
 function ValidatePassword(){
-    const regex = /^(?=.*\d)(?=.*[A-Z])/;
     var password = document.getElementById("passwordInput").value;
     if(password.length < 8){
         document.getElementById("passwordInput")
@@ -9,10 +8,12 @@ function ValidatePassword(){
         document.getElementById("passwordInput")
             .setCustomValidity("No debe tener más de 64 caracteres");
     }
+    // Comprobación de al menos una letra
     else if(!password.match(/(?=.*[a-zA-Z])/)){
         document.getElementById("passwordInput")
             .setCustomValidity("Debe contener al menos una letra");
     }
+    // Comprobación de al menos un dígito
     else if(!password.match(/(?=.*\d)/)){
         document.getElementById("passwordInput")
             .setCustomValidity("Debe contener al menos un dígito");
